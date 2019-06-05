af87bc139391185f075f1e913c885f96c53acd70
Logan Klein <lkleindesigns@gmail.com>
2019-06-05 14:03:51 -0500

    Initial commit

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      {/* Nav goes here */}
+      <div className="Palette-colors">{/* bunch of color boxes */}</div>
+      {/* Footer goes here */}
+    </div>
+  )
+}
+
+export default Palette
