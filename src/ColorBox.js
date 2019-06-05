7c655b408e68d98e732c0fe96e015179a48bf160
Logan Klein <lkleindesigns@gmail.com>
2019-06-05 14:49:57 -0500

    Style ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,16 @@ import "./ColorBox.css"
 
 const ColorBox = ({background, name}) => {
   return (
-    <div style={{background: background}} className="ColorBox">
-      <span>{name}</span>
-      <span>More</span>
+    <div style={{ background }} className='ColorBox'>
+      <div className='copy-container'>
+        <div className='box-content'>
+          <span>{name}</span>
+        </div>
+        <button className='copy-button'>Copy</button>
+      </div>
+      <span className='see-more'>More</span>
     </div>
-  )
+  );
 }
 
 export default ColorBox
