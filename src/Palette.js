9d2a91f4063fcbec04958bfec2f6a24b23680ee7
Logan Klein <lkleindesigns@gmail.com>
2019-06-08 14:56:12 -0500

    Style Color Slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react'
-import ColorBox from './ColorBox'
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-import './Palette.css'
+import React, { useState } from "react";
+import ColorBox from "./ColorBox";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import "./Palette.css";
 
-const Palette = ({palette}) => {
-  const [level, setLevel] = useState(500)
+const Palette = ({ palette }) => {
+  const [level, setLevel] = useState(500);
 
-  const changeLevel = (level) => {
-    setLevel(level)
-  }
+  const changeLevel = level => {
+    setLevel(level);
+  };
 
   const colorBoxes = palette.colors[level].map(color => (
     <ColorBox background={color.hex} name={color.name} />
-  ))
+  ));
   return (
     <div className="Palette">
-      <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel}/>
-      {/* Nav goes here */}
-      <div className="Palette-colors">
-        {colorBoxes}
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={changeLevel}
+        />
       </div>
+      {/* Nav goes here */}
+      <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer goes here */}
     </div>
-  )
-}
+  );
+};
 
-export default Palette
+export default Palette;
