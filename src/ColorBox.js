a6dbd01b8edece7cc657c65a595ae4eac11199fa
Logan Klein <lkleindesigns@gmail.com>
2019-06-21 22:33:02 -0500

    Refactor ColorBox styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,66 @@
 import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { makeStyles } from "@material-ui/styles";
 import { Link } from "react-router-dom";
 import chroma from 'chroma-js'
 import "./ColorBox.css";
 
-const ColorBox = ({ background, name, moreUrl, showLink }) => {
+const styles = makeStyles({
+  ColorBox: {
+    width: "20%",
+    height: props => props.showingFullPalette ? "25%" : "50%",
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-5px",
+    "&:hover button": {
+      opacity: 1
+    }
+  },
+  copyText: {
+    color: ({background}) => chroma(background).luminance() >= 0.7 ? "black" : "white"
+  },
+  colorName: {
+    color: ({background}) => chroma(background).luminance() <= 0.08 ? "white" : "black"
+  },
+  seeMore: {
+    color: ({background}) => chroma(background).luminance() >= 0.7 ? "black" : "white",
+    background: "rgba(255, 255, 255, 0.3)",
+    position: "absolute",
+    border: "none",
+    right: "0px",
+    bottom: "0px",
+    width: "60px",
+    height: "30px",
+    textAlign: "center",
+    lineHeight: "30px",
+    textTransform: "uppercase"
+  },
+  copyButton: {
+    color: ({background}) => chroma(background).luminance() >= 0.7 ? "black" : "white",
+    width: "100px",
+    height: "30px",
+    position: "absolute",
+    display: "inline-block",
+    top: "50%",
+    left: "50%",
+    marginLeft: "-50px",
+    marginTop: "-15px",
+    textAlign: "center",
+    outline: "none",
+    background: "rgba(255, 255, 255, 0.3);",
+    fontSize: "1rem",
+    lineHeight: "30px",
+    textTransform:" uppercase",
+    border: "none",
+    textDecoration: "none",
+    opacity: 0
+  }
+})
+
+const ColorBox = ({ background, name, moreUrl, showingFullPalette }) => {
+  const classes = styles({background, showingFullPalette})
   const [copied, setCopied] = useState(false);
 
   const changeCopyState = () => {
@@ -13,30 +69,27 @@ const ColorBox = ({ background, name, moreUrl, showLink }) => {
       setCopied(false);
     }, 1500);
   };
-  
-  const isDarkColor = chroma(background).luminance() <= 0.08
-  const isLightColor = chroma(background).luminance() >= 0.05
 
   return (
     <CopyToClipboard text={background} onCopy={changeCopyState}>
-      <div style={{ background }} className="ColorBox">
+      <div style={{ background }} className={classes.ColorBox}>
         <div
           style={{ background }}
           className={`copy-overlay ${copied && "show"}`}
         />
         <div className={`copy-msg ${copied && "show"}`}>
           <h1>copied!</h1>
-          <p className={isLightColor ? "dark-text" : undefined}>{background}</p>
+          <p className={classes.copyText}>{background}</p>
         </div>
         <div className="copy-container">
           <div className="box-content">
-            <span className={isDarkColor ? 'light-text' : undefined}>{name}</span>
+            <span className={classes.colorName}>{name}</span>
           </div>
-          <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
+          <button className={classes.copyButton}>Copy</button>
         </div>
-        {showLink && (
+        {showingFullPalette && (
           <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-            <span className={`see-more ${isLightColor && "dark-text"}`}>MORE</span>
+            <span className={classes.seeMore}>MORE</span>
           </Link>
         )}
       </div>
--- src/Palette.js
+++ src/Palette.js
@@ -23,7 +23,7 @@ const Palette = ({ palette }) => {
       name={color.name}
       key={color.id}
       moreUrl={`/palette/${palette.id}/${color.id}`}
-      showLink={true}
+      showingFullPalette
     />
   ));
 
